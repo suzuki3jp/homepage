507bd8338b8ea2a0739499ac2893981130db2542
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-03-15 22:23:46 +0900

    :sparkles: /projectsを追加

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,13 +1,66 @@
 <script>
 	import { Content } from 'carbon-components-svelte';
+	import ProjectCard from './ProjectCard.svelte';
 </script>
 
-<Content>
-	<h1 class="main">Projects</h1>
-</Content>
+<svelte:head>
+	<title>Projects</title>
+	<meta name="description" content="suzuki3jp's projects" />
+</svelte:head>
+<div class="main-content">
+	<Content>
+		<h1 class="text-center" style="padding-top: 2%; font-weight: bold; margin-bottom: 2%;">
+			Projects
+		</h1>
+		<div class="project-card-inline">
+			<ProjectCard
+				title="ArikenCompany"
+				desc="Twitchのチャットボット<br>コメントに関する統計の収集<br>チャットコマンド、その他さまざまな機能を提供する<br><a href='https://nightbot.tv/'>Nightbot</a>をモデルに作成"
+				href="https://arikencompany.github.io/"
+				github="https://github.com/suzuki3jp/arikencompany"
+			/>
+			<ProjectCard
+				title="twitch.js"
+				desc="Twitchのチャットボットを作成するためのライブラリ<br>チャットボットの機能作成に関するUtilも含む<br><a href='https://github.com/twurple/twurple'>twurple</a>を拡張している"
+				github="https://github.com/suzuki3jp/twitch.js"
+				npm="https://www.npmjs.com/package/@suzuki3jp/twitch.js"
+			/>
+		</div>
+		<div class="project-card-inline">
+			<ProjectCard
+				title="suzuki-dev.com"
+				desc="Twitchの配信uptime<br>VALORANTのランクポイント<br>コンペ勝率などを日本語で返すAPI"
+				github="https://github.com/suzuki3jp/suzuki-dev"
+			/>
+			<ProjectCard
+				title="nightbot.js"
+				desc="<a href='https://api-docs.nightbot.tv/'>Nightbot API</a>を操作するためのライブラリ"
+				github="https://github.com/suzuki3jp/nightbot.js"
+			/>
+		</div>
+	</Content>
+</div>
 
 <style>
-	.main {
-		padding-top: 10px;
+	.main-content {
+		width: 70%;
+		margin-right: auto;
+		margin-left: auto;
+		margin-bottom: auto;
+	}
+
+	.project-card-inline {
+		display: flex;
+		margin-top: 5%;
+	}
+
+	@media (max-width: 1055px) {
+		.project-card-inline {
+			display: inline;
+			margin-top: 5%;
+		}
+		.main-content {
+			width: 100%;
+		}
 	}
 </style>
---
+++ src/routes/projects/ProjectCard.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import GitHub from '$lib/images/github.png';
+	import Links from '$lib/images/links-icon.png';
+	import Npm from '$lib/images/npm.png';
+	export let title: string;
+	export let desc: string | undefined = undefined;
+	export let href: string | undefined = undefined;
+	export let github: string | undefined = undefined;
+	export let npm: string | undefined = undefined;
+</script>
+
+<div class="project-card">
+	<h2 class="text-center" style="font-weight: 500; margin: 5px 0 10px 0;">{title}</h2>
+	{#if desc}
+		<div style="margin-left: 5%; font-size: 15px; line-height: 20px">{@html desc}</div>
+	{/if}
+	<div class="project-card-links">
+		{#if href}
+			<a {href} class="project-card-links-margin">
+				<img src={Links} alt="links icon" style="width: 30px;" /></a
+			>
+		{/if}
+		{#if github}
+			<a href={github} class="project-card-links-margin">
+				<img src={GitHub} alt="github logo" style="width: 30px;" />
+			</a>
+		{/if}
+		{#if npm}
+			<a href={npm} class="project-card-links-margin">
+				<img src={Npm} alt="npm logo" style="width: 30px" />
+			</a>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.project-card {
+		border-color: white;
+		border-width: 2px;
+		border-style: solid;
+		width: 50%;
+		background-color: #4c4c4c;
+		padding: 5px 0 5px 0;
+		margin-right: 5%;
+		height: fit-content;
+	}
+
+	.project-card-links {
+		margin-left: auto;
+		width: fit-content;
+		margin-right: 6%;
+		display: flex;
+	}
+
+	.project-card-links-margin {
+		margin-left: 10%;
+	}
+	@media (max-width: 1055px) {
+		.project-card {
+			width: 80%;
+			margin-top: 5%;
+			margin-left: auto;
+			margin-right: auto;
+		}
+	}
+	@media (max-width: 400px) {
+		.project-card {
+			width: 90%;
+			margin-top: 5%;
+			margin-left: auto;
+			margin-right: auto;
+		}
+	}
+</style>
