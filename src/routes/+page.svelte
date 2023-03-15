5cbbf684978a2a56138ba8fd5f8fb4f41ef742e6
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-03-15 22:27:21 +0900

    :zap: homeの表示崩れを修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,8 @@
 	<meta name="description" content="suzuki3jp's homepage" />
 </svelte:head>
 
-<Content class="text-center">
-	<img src={suzuki3jp1000} alt="" style="width: 180px; padding-top: 20%;" />
+<Content class="text-center" style="margin-height: auto; margin-bottom: auto;">
+	<img src={suzuki3jp1000} alt="" style="width: 180px; padding-top: 10%;" />
 	<h1 style="margin-top: 3%; font-weight: bold;">suzuki3jp</h1>
 	<h4 style="font-family:'Yu Gothic', '游ゴシック', YuGothic, '游ゴシック体';">
 		オタクしたりゲームしたりプログラム書いたり
